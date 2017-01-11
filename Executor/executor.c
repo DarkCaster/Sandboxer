f7861fa71410112c1c50098e593f7335b01f71a2
DarkCaster <dark.caster@outlook.com>
2017-01-11 15:57:04 +0300

    Added executor project templates. Using CMake build system.

---
+++ Executor/executor.c
@@ -0,0 +1,19 @@
+#include "config.h"
+#include <string.h>
+//#include <logger.h>
+
+#define MAXARGLEN 256
+
+int main(int argc, char* argv[])
+{
+    if(argc!=2)
+        return -1;
+    if(strnlen(argv[0], MAXARGLEN)>=MAXARGLEN)
+        return -2;
+    if(strnlen(argv[1], MAXARGLEN)>=MAXARGLEN)
+        return -2;
+    const char* input=argv[0];
+    const char* logfile=argv[1];
+
+
+}
