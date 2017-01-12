4a1a302c35656042cd99f085e7e5f2171d56326d
DarkCaster <dark.caster@outlook.com>
2017-01-12 05:32:56 +0300

    executor: added message encoding\decoding logic

--- Executor/message.c
+++ Executor/message.c
@@ -2,4 +2,45 @@
 //perform decode\encode data to\from buffer
 //perform sign\verify of data buffer integrity
 #include "message.h"
+#include <string.h>
 
+//local prototypes to satisfy -Wmissing-prototypes
+void msg_write_pl_len(uint8_t* const buffer, int32_t offset, uint16_t len);
+
+uint16_t msg_get_pl_len(const uint8_t* buffer, int32_t offset)
+{
+    return *((const uint16_t*)(buffer+offset));
+}
+
+void msg_write_pl_len(uint8_t* const buffer, int32_t offset, uint16_t len)
+{
+    *((uint16_t* const)(buffer+offset))=len;
+}
+
+int32_t msg_get_pl_offset(int32_t offset)
+{
+    return offset+MSGHDRSZ;
+}
+
+int32_t msg_decode(uint8_t* const dest, int32_t dest_offset, const uint8_t* src, int32_t src_offset, HSEEDTYPE h_seed)
+{
+    uint16_t pl_len=msg_get_pl_len(src,src_offset);
+    if(pl_len>MSGPLMAXLEN_RAW)
+        return -1;
+    if( !verify_hash(h_seed,src,src_offset+MSGHDRSZ,(int32_t)pl_len) )
+        return -1;
+    int32_t r_len=get_hpl_len((int32_t)pl_len);
+    memcpy((void* const)(dest+dest_offset),(const void*)(src+src_offset+MSGHDRSZ),(size_t)r_len);
+    return r_len;
+}
+
+//return len of data written to dest at dest_offset (-1==error while encoding), write encoded message contents to dest at dest_offset.
+int32_t msg_encode(uint8_t* const dest, int32_t dest_offset, const uint8_t* src, int32_t src_offset, int32_t src_len, HSEEDTYPE h_seed)
+{
+    if(src_len<0 || src_len>MSGPLMAXLEN)
+        return -1;
+    memcpy((void* const)(dest+dest_offset+MSGHDRSZ),(const void*)(src+src_offset),(size_t)src_len);
+    int32_t pl_len=append_hash(h_seed,dest,dest_offset+MSGHDRSZ,src_len);
+    msg_write_pl_len(dest,dest_offset,(uint16_t)pl_len);
+    return pl_len+MSGHDRSZ;
+}
--- Executor/message.h
+++ Executor/message.h
@@ -1,7 +1,8 @@
-#ifndef LOGGER_INCLUDED
-#define LOGGER_INCLUDED
+#ifndef MESSAGE_INCLUDED
+#define MESSAGE_INCLUDED
 
 #include "config.h"
+#include "hash.h"
 
 #ifdef HAVE_INTTYPES_H
     #include <inttypes.h>
@@ -18,6 +19,25 @@
 extern "C" {
 #endif
 
+#define MSGHDRSZ 2
+#define MSGLENTYPE uint16_t
+
+#if DATABUFSZ>=(65535+MSGHDRSZ)
+    #define MSGPLMAXLEN_RAW 65535
+#else
+    #define MSGPLMAXLEN_RAW (DATABUFSZ-MSGHDRSZ)
+#endif
+
+#define MSGPLMAXLEN (MSGPLMAXLEN_RAW-HASHSZ)
+
+//helper methods, used when downloading message+it's payload
+MSGLENTYPE msg_get_pl_len(const uint8_t* buffer, int32_t offset);
+int32_t msg_get_pl_offset(int32_t offset);
+
+//return len of data written to dest at dest_offset (-1==error while decoding), write decoded message contents to dest at dest_offset.
+int32_t msg_decode(uint8_t* const dest, int32_t dest_offset, const uint8_t* src, int32_t src_offset, HSEEDTYPE h_seed);
+//return len of data written to dest at dest_offset (-1==error while encoding), write encoded message contents to dest at dest_offset.
+int32_t msg_encode(uint8_t* const dest, int32_t dest_offset, const uint8_t* src, int32_t src_offset, int32_t src_len, HSEEDTYPE h_seed);
 
 #ifdef __cplusplus
 }
