89c1a8b4f52951ce1c25203cc046e948b2f8b642
DarkCaster <dark.caster@outlook.com>
2017-01-27 18:05:21 +0300

    executor: added pid group holder's logic

---
+++ Executor/pid_group_holder.h
@@ -0,0 +1,16 @@
+#ifndef PID_GROUP_HOLDER_INCLUDED
+#define PID_GROUP_HOLDER_INCLUDED
+
+#include "helper_macro.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+pid_t spawn_pid_holder(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
