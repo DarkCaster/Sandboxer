c3fbc440c66b1f58a9088bee4f4d52d2cb75629d
DarkCaster <dark.caster@outlook.com>
2017-02-01 19:51:38 +0300

    Added sandboxer.sh script template

---
+++ Sandboxer/sandboxer.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+curdir="$PWD"
+script_dir="$( cd "$( dirname "$0" )" && pwd )"
+self=`basename "$0"`
+test ! -e "$script_dir/$self" && echo "script_dir detection failed. cannot proceed!" && exit 1
+script_file=`readlink "$script_dir/$self"`
+test ! -z "$script_file" && script_dir=`realpath \`dirname "$script_file"\``
+
+config="$1"
+test -z "$config" && echo "usage: sandboxer.sh <config file> <exec profile> [other parameters, will be forwarded to executed app]" && exit 1
+shift 1
+
+profile="$1"
+test -z "$profile" && echo "usage: sandboxer.sh <config file> <exec profile> [other parameters, will be forwarded to executed app]" && exit 1
+shift 1
+
+. "$script_dir/find-lua-helper.bash.in"
+. "$bash_lua_helper" "$config" -e sandbox -e profile -b "$script_dir/sandboxer.pre.lua" -a "$script_dir/sandboxer.post.lua" -o "$profile" -o "$script_dir" -o "$curdir" -x "$@"
+
+shift $#
+
+test "${#cfg[@]}" = "0" && echo "can't find config storage variable populated by bash_lua_helper. bash_lua_helper failed!" && exit 1
+
+log () {
+ echo "[ $@ ]"
+}
+
+check_errors () {
+ local status="$?"
+ if [ "$status" != "0" ]; then
+  log "ERROR: last operation finished with error code $status"
+  exit $status
+ fi
+}
+
+
+
