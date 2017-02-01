581bba3b9c57a39b62cdd861ef2ce0d559f59207
DarkCaster <dark.caster@outlook.com>
2017-02-02 02:16:11 +0300

    Added more options to example config file and post checks

--- Sandboxer/example.cfg.lua
+++ Sandboxer/example.cfg.lua
@@ -12,12 +12,23 @@ sandbox =
 		net=false,
 		uts=true,
 		cgroup=false,
-		-- uid=1000,
-		-- gid=1000,
-		-- hostname=sandbox,
+		-- uid=1000, -- variable not defined by default
+		-- gid=1000, -- variable not defined set by default
+		-- hostname=sandbox, -- not set by default
 	},
-
-
+	-- integration options.
+	-- allow applications inside sandbox to use some features from host user session
+	integration =
+	{
+		pulse=true, -- TODO
+		x11=true, -- TODO
+	},
+	-- sandbox features
+	-- some service features or subsystems that applications may use inside sandbox
+	features =
+	{
+		dbus=true, -- TODO
+	},
 }
 
 shell =
--- Sandboxer/sandboxer.post.lua
+++ Sandboxer/sandboxer.post.lua
@@ -1,5 +1,7 @@
 -- sandbox table verification
 assert(type(sandbox)=="table", "sandbox param incorrect")
+
+-- lockdown table
 assert(type(sandbox.lockdown)=="nil" or type(sandbox.lockdown)=="table", "sandbox.lockdown param incorrect")
 if type(sandbox.lockdown)=="nil" then sandbox.lockdown={} end
 assert(type(sandbox.lockdown.user)=="nil" or type(sandbox.lockdown.user)=="boolean", "sandbox.lockdown.user param incorrect")
@@ -8,21 +10,32 @@ assert(type(sandbox.lockdown.pid)=="nil" or type(sandbox.lockdown.pid)=="boolean
 assert(type(sandbox.lockdown.net)=="nil" or type(sandbox.lockdown.net)=="boolean", "sandbox.lockdown.net param incorrect")
 assert(type(sandbox.lockdown.uts)=="nil" or type(sandbox.lockdown.uts)=="boolean", "sandbox.lockdown.uts param incorrect")
 assert(type(sandbox.lockdown.cgroup)=="nil" or type(sandbox.lockdown.cgroup)=="boolean", "sandbox.lockdown.cgroup param incorrect")
-
 if type(sandbox.lockdown.user)=="nil" then sandbox.lockdown.user=true end
 if type(sandbox.lockdown.ipc)=="nil" then sandbox.lockdown.ipc=true end
 if type(sandbox.lockdown.pid)=="nil" then sandbox.lockdown.pid=true end
 if type(sandbox.lockdown.net)=="nil" then sandbox.lockdown.net=false end
 if type(sandbox.lockdown.uts)=="nil" then sandbox.lockdown.uts=true end
 if type(sandbox.lockdown.cgroup)=="nil" then sandbox.lockdown.cgroup=false end
-
 assert(type(sandbox.lockdown.uid)=="nil" or type(sandbox.lockdown.uid)=="number", "sandbox.lockdown.uid param incorrect")
 assert(type(sandbox.lockdown.gid)=="nil" or type(sandbox.lockdown.gid)=="number", "sandbox.lockdown.gid param incorrect")
 assert(type(sandbox.lockdown.hostname)=="nil" or type(sandbox.lockdown.hostname)=="string", "sandbox.lockdown.hostname param incorrect")
-
 if type(sandbox.lockdown.uid)=="number" then sandbox.lockdown.user=true end
 if type(sandbox.lockdown.gid)=="number" then sandbox.lockdown.user=true end
 if type(sandbox.lockdown.hostname)=="string" then sandbox.lockdown.uts=true end
+
+-- integration table
+assert(type(sandbox.integration)=="nil" or type(sandbox.integration)=="table", "sandbox.integration param incorrect")
+if type(sandbox.integration)=="nil" then sandbox.integration={} end
+assert(type(sandbox.integration.pulse)=="nil" or type(sandbox.integration.pulse)=="boolean", "sandbox.integration.pulse param incorrect")
+assert(type(sandbox.integration.x11)=="nil" or type(sandbox.integration.x11)=="boolean", "sandbox.integration.x11 param incorrect")
+if type(sandbox.integration.pulse)=="nil" then sandbox.integration.pulse=true end
+if type(sandbox.integration.x11)=="nil" then sandbox.integration.x11=true end
+
+-- features table
+assert(type(sandbox.features)=="nil" or type(sandbox.features)=="table", "sandbox.features param incorrect")
+if type(sandbox.features)=="nil" then sandbox.features={} end
+assert(type(sandbox.features.dbus)=="nil" or type(sandbox.features.dbus)=="boolean", "sandbox.features.dbus param incorrect")
+if type(sandbox.features.dbus)=="nil" then sandbox.features.dbus=true end
 
 -- load profile, and perform it's verification
 profile=loadstring("return " .. loader.extra[1])()
