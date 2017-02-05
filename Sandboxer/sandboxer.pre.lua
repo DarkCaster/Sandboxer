aa798b49840854df01947891ae3cb3715ad2e58e
DarkCaster <dark.caster@outlook.com>
2017-02-05 03:05:51 +0300

    sandboxer.pre.lua: add signal list

--- Sandboxer/sandboxer.pre.lua
+++ Sandboxer/sandboxer.pre.lua
@@ -14,6 +14,43 @@ config.gid = loader.extra[9]
 
 defaults={}
 
+-- signals list
+defaults.signals=
+{
+SIGHUP=1,
+SIGINT=2,
+SIGQUIT=3,
+SIGILL=4,
+SIGTRAP=5,
+SIGABRT=6,
+SIGIOT=6,
+SIGBUS=7,
+SIGFPE=8,
+SIGKILL=9,
+SIGUSR1=10,
+SIGSEGV=11,
+SIGUSR2=12,
+SIGPIPE=13,
+SIGALRM=14,
+SIGTERM=15,
+SIGSTKFLT=16,
+SIGCHLD=17,
+SIGCONT=18,
+SIGSTOP=19,
+SIGTSTP=20,
+SIGTTIN=21,
+SIGTTOU=22,
+SIGURG=23,
+SIGXCPU=24,
+SIGXFSZ=25,
+SIGVTALRM=26,
+SIGPROF=27,
+SIGWINCH=28,
+SIGIO=29,
+SIGPWR=30,
+SIGSYS=31,
+}
+
 defaults.custom_commands={}
 -- TODO: move this to chroot table (not complete yet)
 
