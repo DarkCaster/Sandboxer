5b3f857f4966b87ce6dfbb23ff815a2a4bcc1946
DarkCaster <dark.caster@outlook.com>
2017-02-10 17:12:10 +0300

    sandboxer configs and run-profile script updated: added new paremeter "term_child_only" to reflect recent changes at executor module

--- Sandboxer/sandboxer.post.lua
+++ Sandboxer/sandboxer.post.lua
@@ -123,6 +123,7 @@ function loader.check_profile(profile, name)
  loader.check_one_level_string_list(profile.env_unset, name..".env_unset")
  loader.check_one_level_env_set_list(profile.env_set, name..".env_set")
  assert(type(profile.term_signal)=="number" or type(profile.term_signal)=="nil", name..".term_signal value is incorrect or missing")
+ assert(type(profile.term_child_only)=="boolean" or type(profile.term_child_only)=="nil", name..".term_child_only value is incorrect or missing")
  assert(type(profile.attach)=="boolean" or type(profile.attach)=="nil", name..".attach value is incorrect or missing")
  if type(profile.attach)=="nil" then profile.attach=false end
  assert(type(profile.pty)=="boolean" or type(profile.pty)=="nil", name..".pty value is incorrect or missing")
