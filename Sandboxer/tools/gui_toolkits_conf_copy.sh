03083f027f1d662954d7985068cd7d60bef7a0fe
DarkCaster <dark.caster@outlook.com>
2017-03-04 02:42:51 +0300

    gui_toolkits_conf_copy.sh: add some logic to copy gtk config

--- Sandboxer/tools/gui_toolkits_conf_copy.sh
+++ Sandboxer/tools/gui_toolkits_conf_copy.sh
@@ -3,3 +3,20 @@
 username="$1"
 userhome_chroot="$2"
 userhome_chroot_hostpath="$3"
+
+# gtkrc files at $HOME directory
+for hint in "$HOME/.gtkrc"*
+do
+  test ! -z "$hint" -a -f "$hint" && 2>/dev/null cp "$hint" "$userhome_chroot_hostpath"
+  test ! -z "$hint" -a -d "$hint" && 2>/dev/null cp -rf "$hint" "$userhome_chroot_hostpath"
+done
+
+mkdir -p "$userhome_chroot_hostpath/.config"
+
+for hint in "$HOME/.config/gtk"*
+do
+  test ! -z "$hint" -a -f "$hint" && 2>/dev/null cp "$hint" "$userhome_chroot_hostpath/.config"
+  test ! -z "$hint" -a -d "$hint" && 2>/dev/null cp -rf "$hint" "$userhome_chroot_hostpath/.config"
+done
+
+exit 0
