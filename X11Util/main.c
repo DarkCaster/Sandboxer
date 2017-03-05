6d160533b39fb5017dee6f286bbd8cfd8e91f672
DarkCaster <dark.caster@outlook.com>
2017-03-06 02:12:32 +0300

    x11util: sys.v hack updated, added some debug, added more notes. Added code templates for main logic.

--- X11Util/main.c
+++ X11Util/main.c
@@ -1,20 +1,35 @@
 #include "helper_macro.h"
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include <stdio.h>
 
-static void show_usage(void)
-{
-    fprintf(stderr,"Usage: <mode number>\n");
-    fprintf(stderr,"  Currently supported modes:\n");
-    fprintf(stderr,"    0 - Perform simple test/hack with Sys.V SHM (may be needed for MIT-SHM X11 extension to work with sandbox's IPC isolation), and test x11 connection.\n");
-    fprintf(stderr,"    1 - (TODO) Same as 0, but echo (to stdout) X11 env setup needed for clients to work. Then open X11 connection and wait forever for term signal. For use with some X11 forwarding and isolation software (like xpra).\n");
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MAXARGLEN 4095
+
+int mode_0(void);
+int mode_1(void);
+
+static void show_usage(void) {
+    fputs("Usage: <mode number>\n",stderr);
+    fputs("  Currently supported modes:\n",stderr);
+    fputs("    0 - Perform simple test/hack with Sys.V SHM (may be needed for MIT-SHM X11 extension to work with sandbox's IPC isolation), and test x11 connection.\n",stderr);
+    fputs("    1 - (TODO) Same as 0, but echo (to stdout) X11 env setup needed for clients to work. Then open X11 connection and wait forever for term signal. For use with some X11 forwarding and isolation software (like xpra).\n",stderr);
     exit(1);
 }
 
-int main(int argc, char* argv[])
+static bool arg_is_numeric(const char* arg)
 {
-    if(argc!=2)
+    size_t len=strnlen(arg,MAXARGLEN);
+    for(size_t i=0;i<len;++i)
+        if(!isdigit((int)arg[i]))
+            return false;
+    return true;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2 || !arg_is_numeric(argv[1]))
         show_usage();
     /*
      * This is a simple hack, that tries to address this problem:
@@ -25,14 +40,61 @@ int main(int argc, char* argv[])
      * "0" is NOT an invalid shm segment ID ("-1" is), but in normal case it is almost never happened that this call return "0".
      * But, ocassionaly, it happens on the first call to shmget when using IPC namespace isolation (sandboxing software like docker or bwrap do that)
      *
-     * Maybe this is a bug somewhere at x11, or other lower level GUI code (return code from shmget compered with 0 insted of -1).
+     * Maybe this is a bug somewhere at x11, or other lower level GUI code (return code from shmget compered with 0 insted of -1 ?).
      * Or maybe (likely) my investigation is wrong. But the folowing simple hack that i've develoved, seems to work.
      * It simply allocates shm segments several times to increase ID number returned by shmget calls.
      * So, when this call performed later by real gui application, first call to shmget will not return 0.
      */
-    int seg=shmget(IPC_PRIVATE, 4096, IPC_CREAT|0600);
-    const void* ptr=shmat(seg, NULL, 0);
-    shmctl(seg, IPC_RMID, NULL);
-    shmdt(ptr);
+
+    //Some more thoughts: if you set loop limit, say, to 500000, you can see how segment id counter overflows.
+    //Right after overflow, ID is always set to "1". I've never seen, that it set to "0" after overflow!
+    int last_seg = -1;
+    for (int i = 0; i < 50 /*500000*/; ++i)
+    {
+        int seg = shmget(IPC_PRIVATE, 4096, IPC_CREAT | 0600);
+        if(seg <= last_seg && seg != -1)
+            fprintf(stderr, "shmget overflow. ID: %d\n", seg);
+        if(seg == -1)
+        {
+            perror("shmget reports error:");
+            continue;
+        }
+        last_seg = seg;
+        const void* ptr = shmat(seg, NULL, 0);
+        if(ptr == (void*)-1)
+        {
+            perror("shmat reports error:");
+            continue;
+        }
+        if(shmctl(seg, IPC_RMID, NULL) == -1)
+        {
+            perror("shmctl reports error:");
+            continue;
+        }
+        if(shmdt(ptr) == -1)
+            perror("shmdt reports error:");
+    }
+    int mode=(int)strtol(argv[1], NULL, 10);
+    if(mode>1)
+        mode=1;
+    if(mode==0)
+       return mode_0();
+    else
+       return mode_1();
+}
+
+int mode_0(void)
+{
+    //TODO: varoius x11 tests
     return 0;
+}
+
+int mode_1(void)
+{
+    int ec=mode_0();
+    if(ec!=0)
+        return ec;
+    //TODO: connect to x11, echo env to stdout, wait for sigterm.
+    fputs("mode_1: not implemented!",stderr);
+    return 1;
 }
