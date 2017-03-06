27adfd4301cd3a16abb9d4243a2864a2fc56973c
DarkCaster <dark.caster@outlook.com>
2017-03-06 03:35:19 +0300

    x11util: added some dependencies check to cmake script

--- X11Util/xshm_test.c
+++ X11Util/xshm_test.c
@@ -1,10 +1,15 @@
 #include "helper_macro.h"
+
+#include <X11/Xlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <X11/extensions/XShm.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char* argv[])
 {
-
     return (EXIT_SUCCESS);
 }
 
