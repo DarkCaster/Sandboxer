7e512e02e12928bcbf536697d135603dad68ae2d
DarkCaster <dark.caster@outlook.com>
2017-03-10 02:07:24 +0300

    Added build-bwrap.sh script to download, build and install bwrap utility to /usr/local

---
+++ build-bwrap.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Download, build bwrap utility and install it to /usr/local
+# Require root priveleges, will ask for password (user or root), at install stage
+
+curdir="$( cd "$( dirname "$0" )" && pwd )"
+
+function check_error {
+  if [ "$?" != "0" ]; then
+    echo "Build ended with error !!!"
+    cd "$curdir"
+    exit 1
+  fi
+}
+
+cd "$curdir"
+
+if [ ! -d "$curdir/External/Bwrap" ]; then
+  git clone https://github.com/projectatomic/bubblewrap.git "$curdir/External/Bwrap"
+  check_error
+fi
+
+cd "$curdir/External/Bwrap"
+check_error
+
+&>/dev/null make distclean
+
+NOCONFIGURE=YES ./autogen.sh
+check_error
+
+rm -rf "$curdir/Build/Bwrap-build"
+mkdir -p "$curdir/Build/Bwrap-build" && cd "$curdir/Build/Bwrap-build"
+check_error
+
+"$curdir/External/Bwrap/configure" --prefix="/usr/local" --with-priv-mode=setuid
+check_error
+
+make
+check_error
+
+sudo make install
+check_error
