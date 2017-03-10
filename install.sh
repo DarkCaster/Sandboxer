8cc616e512025e4e18b33dd66145625a17ee21b1
DarkCaster <dark.caster@outlook.com>
2017-03-10 03:36:12 +0300

    Added install.sh - install sandboxer suite locally to user's home directory

---
+++ install.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# Install sandboxer suite to local user home directory
+# You must build all required binaries before running this script: run build.sh and build-bwrap.sh (optionally).
+
+target="$1"
+test -z "$target" && target="$HOME/sandboxer"
+test "$target" = "$HOME" && echo "will not proceed install to home directory directly, try $HOME/sandboxer sub-directory instead"
+
+curdir="$( cd "$( dirname "$0" )" && pwd )"
+
+function check_error {
+ if [ "$?" != "0" ]; then
+  echo "Install ended with error !!!"
+  cd "$curdir"
+  exit 1
+ fi
+}
+
+echo "Installing sandboxer suite to $target directory"
+
+mkdir -p "$target"
+check_error
+
+rm -rf "$target/bin"
+
+mkdir -p "$target/examples"
+check_error
+
+mkdir -p "$target/bin"
+check_error
+
+# install main scripts
+echo "Installing main scripts"
+
+cp "$curdir/Sandboxer/sandboxer.sh" "$target/bin"
+check_error
+
+cp "$curdir/Sandboxer/sandboxer.pre.lua" "$target/bin"
+check_error
+
+cp "$curdir/Sandboxer/sandboxer.post.lua" "$target/bin"
+check_error
+
+cp -r "$curdir/Sandboxer/fixups" "$target/bin"
+check_error
+
+cp -r "$curdir/Sandboxer/includes" "$target/bin"
+check_error
+
+cp -r "$curdir/Sandboxer/tools" "$target/bin"
+check_error
+
+echo "Installing service binaries"
+
+cp -r "$curdir/Build/commander" "$target/bin"
+check_error
+
+cp -r "$curdir/Build/executor" "$target/bin"
+check_error
+
+cp -r "$curdir/Build/x11util" "$target/bin"
+check_error
+
+cp -r "$curdir/Build/fixups" "$target/bin"
+check_error
+
+"$curdir/BashLuaHelper/install.sh" "$target/bin/BashLuaHelper"
+check_error
+
+# copy examples
+
+echo "Installing examples"
+
+for example in "$target/examples"/*.cfg.lua
+do
+  test ! -f "$example" && continue
+  echo "Moving existing example config $example to $example.bak"
+  mv "$example" "$example.bak"
+  check_error
+done
+
+for example in "$curdir/Examples"/*.cfg.lua
+do
+  test ! -f "$example" && continue
+  cp "$example" "$target/examples"
+  check_error
+done
