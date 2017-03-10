8b7cd5f43c7c721f58d86e9c9a16a63f867d9cfa
DarkCaster <dark.caster@outlook.com>
2017-03-10 17:01:23 +0300

    Minor refactor at download-ubuntu-* service scripts

--- Examples/download-ubuntu-12.04-chroot.sh
+++ Examples/download-ubuntu-12.04-chroot.sh
@@ -3,16 +3,16 @@
 script_dir="$( cd "$( dirname "$0" )" && pwd )"
 
 check_errors () {
- local status="$?"
- local msg="$@"
- if [ "$status" != "0" ]; then
-  echo "ERROR: operation finished with error code $status"
-  test ! -z "$msg" && echo "$msg"
-  exit "$status"
- fi
+  local status="$?"
+  local msg="$@"
+  if [ "$status" != "0" ]; then
+    echo "ERROR: operation finished with error code $status"
+    [[ ! -z $msg ]] && echo "$msg"
+    exit "$status"
+  fi
 }
 
-test ! -d "$script_dir/ubuntu_chroot" || check_errors "directory $script_dir/ubuntu_chroot already exist!"
+[[ ! -d $script_dir/ubuntu_chroot ]] || check_errors "directory $script_dir/ubuntu_chroot already exist!"
 
 wget -O /tmp/ubuntu-root.tar.gz https://partner-images.canonical.com/core/precise/current/ubuntu-precise-core-cloudimg-amd64-root.tar.gz
 check_errors
