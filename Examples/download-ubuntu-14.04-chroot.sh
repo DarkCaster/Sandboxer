6ca1e857eb065fe12530fff3624c3dd9bc008f7c
DarkCaster <dark.caster@outlook.com>
2017-03-11 22:30:44 +0300

    Added download-ubuntu-14.04-chroot.sh - download and unpack Ubuntu Trusty Tahr minimal rootfs

---
+++ Examples/download-ubuntu-14.04-chroot.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+script_dir="$( cd "$( dirname "$0" )" && pwd )"
+
+check_errors () {
+  local status="$?"
+  local msg="$@"
+  if [[ $status != 0 ]]; then
+    echo "ERROR: operation finished with error code $status"
+    [[ ! -z $msg ]] && echo "$msg"
+    exit "$status"
+  fi
+}
+
+[[ ! -d $script_dir/ubuntu_chroot ]] || check_errors "directory $script_dir/ubuntu_chroot already exist!"
+
+wget -O /tmp/ubuntu-root.tar.gz https://partner-images.canonical.com/core/trusty/current/ubuntu-trusty-core-cloudimg-amd64-root.tar.gz
+check_errors
+
+mkdir -p "$script_dir/ubuntu_chroot"
+check_errors
+
+cd "$script_dir/ubuntu_chroot"
+check_errors
+
+gunzip -c /tmp/ubuntu-root.tar.gz | tar xf - --no-same-owner --preserve-permissions --exclude='dev'
+check_errors
+
+mv "$script_dir/ubuntu_chroot/etc" "$script_dir/ubuntu_chroot/etc_orig"
+check_errors
+
+rm /tmp/ubuntu-root.tar.gz
+check_errors
