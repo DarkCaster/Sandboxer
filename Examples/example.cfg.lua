4a8c1749e2f9fe856298d76bfc2deca84da5893e
DarkCaster <dark.caster@outlook.com>
2017-03-14 17:18:22 +0300

    feature-pre-pulse.sh.in: use special values from tunables.features.pulse_env_alsa_config parameter to skip or unset ALSA_CONFIG_PATH, example.cfg.lua updated

--- Examples/example.cfg.lua
+++ Examples/example.cfg.lua
@@ -88,6 +88,7 @@
 -- tunables.features.x11util_build
 -- tunables.features.x11util_enable
 -- tunables.features.pulse_force_disable_shm -- see this bug (at the end, it is a bug with pid namespaces): https://bugs.freedesktop.org/show_bug.cgi?id=92141
+-- tunables.features.pulse_env_alsa_config -- path to alsa config file, special values: "skip", "unset"
 
 -- if you changed ANY of tunable defaults (above), you MUST run defaults.recalculate() function here,
 -- this will update and recalculate all deps used by other defaults definitions and setup commands.
