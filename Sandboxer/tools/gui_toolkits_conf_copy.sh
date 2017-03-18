5fdf6701cedb8241c40e4ba6012d7876c5522c22
DarkCaster <dark.caster@outlook.com>
2017-03-18 03:58:23 +0300

    sandboxer: gui_toolkits_conf_copy.sh updated - do not copy gtk config files that may leak some info from host

--- Sandboxer/tools/gui_toolkits_conf_copy.sh
+++ Sandboxer/tools/gui_toolkits_conf_copy.sh
@@ -15,8 +15,19 @@ mkdir -p "$userhome_chroot_hostpath/.config"
 
 for hint in "$HOME/.config/gtk"*
 do
-  [[ ! -z $hint && -f $hint ]] && 2>/dev/null cp "$hint" "$userhome_chroot_hostpath/.config"
-  [[ ! -z $hint && -d $hint ]] && 2>/dev/null cp -rf "$hint" "$userhome_chroot_hostpath/.config"
+  [[ ! -z $hint && -f $hint ]] && 2>/dev/null cp "$hint" "$userhome_chroot_hostpath/.config" && continue
+  if [[ ! -z $hint && -d $hint ]]; then
+    dest="$userhome_chroot_hostpath/.config/`basename \"$hint\"`"
+    mkdir -p "$dest"
+    for sub in "$hint/"*
+    do
+      [[ -z $sub ]] && continue
+      sub_base=`basename "$sub"`
+      [[ $sub_base = bookmarks || $sub_base = gtkfilechooser.ini ]] && continue
+      [[ -f $sub ]] && cp "$sub" "$dest" && continue
+      [[ -d $sub ]] && cp -rf "$sub" "$dest"
+    done
+  fi
 done
 
 exit 0
