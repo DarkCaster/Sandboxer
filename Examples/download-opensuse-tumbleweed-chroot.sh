41d8f68629751e63e43ff98375afe5da2a496a79
DarkCaster <dark.caster@outlook.com>
2017-04-07 19:44:04 +0300

    download-opensuse-*.sh scripts updated: minor addition to previous commit

--- Examples/download-opensuse-tumbleweed-chroot.sh
+++ Examples/download-opensuse-tumbleweed-chroot.sh
@@ -25,8 +25,9 @@ rm -f "$script_dir/opensuse_chroot/etc/machine-id"
 check_errors
 
 #echo some notes
+echo "***********************************************"
 echo "opensuse_chroot directory preparation complete."
 echo "it is recommended to install some packages that will make this stripped down docker-based chroot more suitable for regular usage"
 echo "launch fakeroot_shell with this command \"sandboxer.sh opensuse-setup.cfg.lua fakeroot_shell\""
-echo "then execute \"zypper ref --force; zypper install dbus-1 aaa_base fipscheck glibc-locale ncurses-utils udev\""
+echo "then execute \"zypper ref --force; zypper install dbus-1 aaa_base fipscheck glibc-locale ncurses-utils udev psmisc\""
 echo "if you want to use nice yast2 package manager with GUI, execute \"zypper install yast2-packager yast2-country yast2-fonts yast2-online-update-frontend yast2-online-update yast2-x11 yast2-qt yast2-qt-branding-openSUSE libyui-qt-pkg libyui-qt-graph xorg-x11-fonts gnu-free-fonts\""
