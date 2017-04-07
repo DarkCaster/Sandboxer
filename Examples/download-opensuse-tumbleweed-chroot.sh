28c90f3268ce2d779d9e2c15bb6a70f2040be876
DarkCaster <dark.caster@outlook.com>
2017-04-07 19:19:51 +0300

    download-opensuse-*.sh scripts updated: add some fixes and notes

--- Examples/download-opensuse-tumbleweed-chroot.sh
+++ Examples/download-opensuse-tumbleweed-chroot.sh
@@ -17,6 +17,16 @@ check_errors () {
 "$script_dir/download-image-from-docker-repo.sh" opensuse tumbleweed openSUSE-Tumbleweed.tar.xz
 check_errors "download-image-from-docker-repo.sh script failed!"
 
+#remove "installrecommends = no" for zypper, that was defined on image builds
+sed -i -e 's|^installRecommends.*$|# installRecommends = yes|g' "$script_dir/opensuse_chroot/etc/zypp/zypper.conf"
+
 #remove machine-id, will be generated automatically
-rm -f "$script_dir/debian_chroot/etc/machine-id"
+rm -f "$script_dir/opensuse_chroot/etc/machine-id"
 check_errors
+
+#echo some notes
+echo "opensuse_chroot directory preparation complete."
+echo "it is recommended to install some packages that will make this stripped down docker-based chroot more suitable for regular usage"
+echo "launch fakeroot_shell with this command \"sandboxer.sh opensuse-setup.cfg.lua fakeroot_shell\""
+echo "then execute \"zypper ref --force; zypper install dbus-1 aaa_base fipscheck glibc-locale ncurses-utils udev\""
+echo "if you want to use nice yast2 package manager with GUI, execute \"zypper install yast2-packager yast2-country yast2-fonts yast2-online-update-frontend yast2-online-update yast2-x11 yast2-qt yast2-qt-branding-openSUSE libyui-qt-pkg libyui-qt-graph xorg-x11-fonts gnu-free-fonts\""
