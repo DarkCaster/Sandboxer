66b9d555ca012bae52ebee468110092ed888c7d9
DarkCaster <dark.caster@outlook.com>
2017-04-08 03:34:26 +0300

    download-opensuse-*.sh scripts updated: generate bootstrap scripts to install packages for minimum-usable system

--- Examples/download-opensuse-42.2-chroot.sh
+++ Examples/download-opensuse-42.2-chroot.sh
@@ -24,6 +24,14 @@ sed -i -e 's|^installRecommends.*$|# installRecommends = yes|g' "$script_dir/ope
 rm -f "$script_dir/opensuse_chroot/etc/machine-id"
 check_errors
 
+echo "#!/bin/sh" > "$script_dir/opensuse_chroot/root/bootstrap-minimal.sh"
+echo "zypper ref --force; zypper install dbus-1 aaa_base fipscheck glibc-locale ncurses-utils udev psmisc" >> "$script_dir/opensuse_chroot/root/bootstrap-minimal.sh"
+chmod 755 "$script_dir/opensuse_chroot/root/bootstrap-minimal.sh"
+
+echo "#!/bin/sh" > "$script_dir/opensuse_chroot/root/bootstrap-yast2.sh"
+echo "zypper ref --force; zypper install yast2-packager yast2-country yast2-fonts yast2-online-update-frontend yast2-online-update yast2-x11 yast2-qt yast2-qt-branding-openSUSE libyui-qt-pkg libyui-qt-graph xorg-x11-fonts gnu-free-fonts" >> "$script_dir/opensuse_chroot/root/bootstrap-yast2.sh"
+chmod 755 "$script_dir/opensuse_chroot/root/bootstrap-yast2.sh"
+
 #echo some notes
 echo "***********************************************"
 echo "opensuse_chroot directory preparation complete."
@@ -31,3 +39,4 @@ echo "it is recommended to install some packages that will make this stripped do
 echo "launch fakeroot_shell with this command \"sandboxer.sh opensuse-setup.cfg.lua fakeroot_shell\""
 echo "then execute \"zypper ref --force; zypper install dbus-1 aaa_base fipscheck glibc-locale ncurses-utils udev psmisc\""
 echo "if you want to use nice yast2 package manager with GUI, execute \"zypper install yast2-packager yast2-country yast2-fonts yast2-online-update-frontend yast2-online-update yast2-x11 yast2-qt yast2-qt-branding-openSUSE libyui-qt-pkg libyui-qt-graph xorg-x11-fonts gnu-free-fonts\""
+echo "i have placed bootstrap-minimal.sh and bootstrap-yast2.sh scripts with this commands at chroot /root directory"
