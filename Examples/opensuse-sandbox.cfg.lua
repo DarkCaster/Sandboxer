9201ba2ca9b71fad2a137e4ca2c16faf21a1d384
DarkCaster <dark.caster@outlook.com>
2017-04-08 03:11:14 +0300

    Added opensuse-sandbox.cfg.lua example config: opensuse sandbox based on external chroot previously setup with opensuse-setup.cfg.lua

---
+++ Examples/opensuse-sandbox.cfg.lua
@@ -0,0 +1,95 @@
+-- this is an example config for sandbox that use external opensuse rootfs as base.
+-- config based on ubuntu-sandbox.cfg.lua, and will be maintained as small as possible - most notes removed.
+-- see ubuntu-sandbox.cfg.lua and example.cfg.lua for more comments and information about config options
+
+-- this example config is compatible with external root-fs archives that was downloaded and extracted by running:
+-- download-opensuse-42.2-chroot.sh - download opensuse 42.2 x86_64 distribution from docker repository
+-- download-opensuse-tumbleweed-chroot.sh - download opensuse tumbleweed x86_64 distribution from docker repository
+
+-- THIS CONFIG WILL CREATE REGULAR SANDBOXED ENV FROM CHROOT DIRECTORY, THAT WAS PREVIOUSLY SETUP WITH opensuse-setup.cfg.lua.
+-- it is strongly recommended to use this config rather than opensuse-setup.cfg.lua to run regular software, most of desktop integration options enabled by default with this config.
+
+tunables.chrootdir=loader.path.combine(loader.workdir,"opensuse_chroot")
+tunables.etchost_path=loader.path.combine(tunables.chrootdir,"etc")
+tunables.features.dbus_search_prefix=tunables.chrootdir
+tunables.features.gvfs_fix_search_prefix=tunables.chrootdir
+tunables.features.x11util_build="opensuse-42.2"
+tunables.features.pulse_env_alsa_config="skip"
+defaults.recalculate()
+
+sandbox={
+  features={
+    "dbus",
+    "gvfs_fix",
+    "pulse",
+    "x11host",
+    "envfix",
+  },
+  setup={
+    executor_build="opensuse-42.2",
+    commands={
+      defaults.commands.machineid_static,
+      defaults.commands.passwd,
+      defaults.commands.resolvconf,
+      defaults.commands.home,
+      defaults.commands.home_gui_config,
+      defaults.commands.var_cache,
+      defaults.commands.var_tmp,
+    },
+    env_whitelist={
+      "LANG",
+      "LC_ALL",
+    },
+    env_set={
+      {"PATH","/usr/local/bin:/usr/bin:/bin:/usr/games:/usr/lib/mit/bin:/usr/lib/mit/sbin"},
+      defaults.env.set_xdg_runtime,
+      defaults.env.set_home,
+    },
+    mounts={
+      defaults.mounts.system_group,
+      defaults.mounts.xdg_runtime_dir,
+      defaults.mounts.home_mount,
+      defaults.mounts.var_cache_mount,
+      defaults.mounts.var_tmp_mount,
+      defaults.mounts.var_lib_mount,
+      defaults.mounts.bin_ro_mount,
+      defaults.mounts.usr_ro_mount,
+      defaults.mounts.lib_ro_mount,
+      defaults.mounts.lib64_ro_mount,
+      defaults.mounts.sbin_ro_mount,
+      {prio=10,"ro-bind",loader.path.combine(tunables.chrootdir,"srv"),"/srv"},
+      {prio=10,"ro-bind",loader.path.combine(tunables.chrootdir,"opt"),"/opt"},
+      defaults.mounts.host_etc_mount,
+      defaults.mounts.passwd_mount,
+      defaults.mounts.machineid_mount,
+      defaults.mounts.resolvconf_mount,
+      defaults.mounts.devsnd_mount,
+      defaults.mounts.devdri_mount,
+      defaults.mounts.devinput_mount,
+      defaults.mounts.sys_mount,
+    },
+  },
+
+  bwrap={
+    defaults.bwrap.unshare_user,
+    -- defaults.bwrap.unshare_ipc,
+    defaults.bwrap.unshare_pid,
+    -- defaults.bwrap.unshare_net,
+    defaults.bwrap.unshare_uts,
+    -- defaults.bwrap.unshare_cgroup,
+    defaults.bwrap.uid,
+    defaults.bwrap.gid,
+  }
+}
+
+shell={
+  exec="/bin/bash",
+  args={"-l"},
+  path="/",
+  env_set={
+    {"TERM",os.getenv("TERM")},
+  },
+  term_signal=defaults.signals.SIGHUP,
+  attach=true,
+  pty=true,
+}
