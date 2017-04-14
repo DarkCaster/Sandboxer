004e0361e3b5782adab968682aae109bdf5b557f
DarkCaster <dark.caster@outlook.com>
2017-04-14 03:21:03 +0300

    Added example config: firefox-debian-sandbox.cfg.lua

---
+++ Examples/firefox-debian-sandbox.cfg.lua
@@ -0,0 +1,114 @@
+-- example config for firefox sandbox, which is created on top of external debian chroot, prepared by debian-setup.cfg.lua
+-- xpra x11-forwarding software (must be installed on host, v2.0 and up) may be used to isolate sanbox from host x11 service.
+-- opengl acceleration untested and may not work (especially with xpra mode or when using proprietary video drivers that install it's own libgl).
+-- this config is based on example.cfg.lua, most comments removed.
+
+tunables.chrootdir=loader.path.combine(loader.workdir,"debian_chroot")
+tunables.datadir=loader.path.combine(loader.workdir,"userdata-firefox")
+tunables.etchost_path=loader.path.combine(tunables.chrootdir,"etc")
+tunables.features.dbus_search_prefix=tunables.chrootdir
+tunables.features.xpra_search_prefix=tunables.chrootdir
+tunables.features.gvfs_fix_search_prefix=tunables.chrootdir
+-- use different build of x11 util, if you experience problems, for example:
+-- tunables.features.x11util_build="debian-8"
+tunables.features.pulse_env_alsa_config="skip"
+defaults.recalculate()
+
+sandbox={
+  features={
+    "dbus",
+    "gvfs_fix",
+    "pulse",
+    --"x11host", -- less secure, try this if you do not have xpra software
+    "xpra", -- more secure, you must install xpra software suite with server and client functionality.
+    "envfix",
+  },
+  setup={
+    executor_build="debian-8",
+    commands={
+      defaults.commands.resolvconf,
+      defaults.commands.machineid_static,
+      defaults.commands.passwd,
+      defaults.commands.home,
+      defaults.commands.home_gui_config,
+      defaults.commands.var_cache,
+      defaults.commands.var_tmp,
+      {'mkdir -p "${cfg[tunables.auto.user_path]}/libs/i386"'},
+      {'mkdir -p "${cfg[tunables.auto.user_path]}/libs/x86_64"'},
+    },
+    env_whitelist={
+      "LANG",
+      "LC_ALL",
+    },
+    env_set={
+      {"PATH","/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games"},
+      defaults.env.set_xdg_runtime,
+      defaults.env.set_home,
+    },
+    mounts={
+      defaults.mounts.system_group, -- includes: proc, dev, empty /run dir, empty /var dir, empty /tmp
+      defaults.mounts.xdg_runtime_dir,
+      defaults.mounts.home_mount,
+      defaults.mounts.var_cache_mount,
+      defaults.mounts.var_tmp_mount,
+      defaults.mounts.var_lib_mount,
+      defaults.mounts.bin_ro_mount,
+      --defaults.mounts.sbin_ro_mount,
+      defaults.mounts.usr_ro_mount,
+      defaults.mounts.lib_ro_mount,
+      defaults.mounts.lib64_ro_mount,
+      defaults.mounts.host_etc_mount,
+      defaults.mounts.passwd_mount,
+      defaults.mounts.machineid_mount,
+      defaults.mounts.resolvconf_mount,
+      --defaults.mounts.devsnd_mount, -- for alsa support.
+      --defaults.mounts.devdri_mount, -- may be needed when using x11host for opengl acceleration
+      --defaults.mounts.sys_mount, -- may be needed when using x11host for opengl acceleration
+      --defaults.mounts.devinput_mount, -- joystics
+      --defaults.mounts.devshm_mount,
+    },
+  },
+
+  bwrap={
+    defaults.bwrap.unshare_user,
+    defaults.bwrap.unshare_ipc,
+    defaults.bwrap.unshare_pid,
+    -- defaults.bwrap.unshare_net,
+    defaults.bwrap.unshare_uts,
+    -- defaults.bwrap.unshare_cgroup,
+    defaults.bwrap.uid,
+    defaults.bwrap.gid,
+  }
+}
+
+shell={
+  exec="/bin/bash",
+  args={"-l"},
+  path="/",
+  env_set={
+    {"TERM",os.getenv("TERM")},
+  },
+  term_signal=defaults.signals.SIGHUP,
+  attach=true,
+  pty=true,
+}
+
+firefox_log={
+  exec="/usr/bin/firefox",
+  path="/home/sandboxer",
+  term_signal=defaults.signals.SIGTERM,
+  attach=true,
+  pty=false,
+  exclusive=true, -- for now it is needed for logging to work
+  log_stderr=loader.path.combine(loader.workdir,"firefox_dbg.err.log"),
+  log_stdout=loader.path.combine(loader.workdir,"firefox_dbg.out.log"),
+}
+
+firefox={
+  exec="/usr/bin/firefox",
+  path="/home/sandboxer",
+  args=loader.args,
+  term_signal=defaults.signals.SIGTERM,
+  attach=false,
+  pty=false,
+}
