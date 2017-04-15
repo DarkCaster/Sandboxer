ca6a23d1fb9794f5fa30b79460148c9e48695f00
DarkCaster <dark.caster@outlook.com>
2017-04-15 19:53:01 +0300

    update_shebang.sh: add shebang update logic, skip files that does not require update

--- update_shebang.sh
+++ update_shebang.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 target="$1"
+
 bash_bin=`2>/dev/null which bash`
 dash_bin=`2>/dev/null which dash`
-echo "Updating script shebang for: $target"
+
+[[ -z $bash_bin ]] && exit 0
+[[ -z $dash_bin ]] && dash_bin="#!/bin/sh"
+
+shebang=`head -n1 "$target"`
+
+if [[ $shebang = "#!/bin/bash" ]]; then
+  result="#!$bash_bin"
+elif [[ $shebang = "#!/bin/dash" ]]; then
+  result="#!$dash_bin"
+elif [[ $shebang = "#!/bin/sh" ]]; then
+  echo "Skipping shebang update for file: $target"
+  exit 1
+else
+  echo "Unknown shebang \"$shebang\" detected: $target"
+  exit 1
+fi
+
+[[ $shebang = $result ]] && exit 0
+
+echo "Updating shebang to \"$result\" for file: $target"
+sed -i '1s|.*|'"$result"'|' "$target"
 
