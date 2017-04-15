5bb898227b3c7b4ec2c170fb69baca2fe3e06296
DarkCaster <dark.caster@outlook.com>
2017-04-16 00:12:11 +0300

    sandboxer: xpra_client.sh tool script updated - add use of optional ipc_file

--- Sandboxer/tools/xpra_client.sh
+++ Sandboxer/tools/xpra_client.sh
@@ -1,14 +1,20 @@
 #!/bin/dash
 base_dir="$1"
 enable_tray="$2"
+ipc_file="$3"
 test -z "$enable_tray" && enable_tray=yes
 test "$enable_tray" = "true" && enable_tray=yes
 test "$enable_tray" = "false" && enable_tray=no
 test -z "$base_dir" && exit 1
 trap "{ rm -f "$base_dir/xpra-client.pid"; }" EXIT
 trap "{ echo \"xpra_client.sh: trap triggered, ignoring\"; }" INT HUP
-1>"$base_dir/xpra-client.stdout" 2>"$base_dir/xpra-client.stderr" \
-xpra attach --socket-dir="$base_dir/control/xpra" --compressors= --compress=0 --encoding=rgb --speed=100 --tray=$enable_tray &
+if test -z "$ipc_file"; then
+  1>"$base_dir/xpra-client.stdout" 2>"$base_dir/xpra-client.stderr" \
+  xpra attach --socket-dir="$base_dir/control/xpra" --compressors= --compress=0 --encoding=rgb --speed=100 --tray=$enable_tray &
+else
+  1>"$base_dir/xpra-client.stdout" 2>"$base_dir/xpra-client.stderr" \
+  xpra attach --socket-dir="$base_dir/control/xpra" --mmap="$ipc_file" --compressors= --compress=0 --encoding=rgb --speed=100 --tray=$enable_tray &
+fi
 xpra_pid="$!"
 echo "$xpra_pid" > "$base_dir/xpra-client.pid"
 wait "$xpra_pid"
