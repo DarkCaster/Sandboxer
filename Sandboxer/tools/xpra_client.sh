80c6ff15c25238b68e9247f0f1a3d4e5ea454058
DarkCaster <dark.caster@outlook.com>
2017-04-15 03:35:04 +0300

    xpra_client.sh: change #!/bin/sh shebang to #!/bin/dash, minor fix

--- Sandboxer/tools/xpra_client.sh
+++ Sandboxer/tools/xpra_client.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/dash
 base_dir="$1"
 enable_tray="$2"
 test -z "$enable_tray" && enable_tray=yes
@@ -6,7 +6,7 @@ test "$enable_tray" = "true" && enable_tray=yes
 test "$enable_tray" = "false" && enable_tray=no
 test -z "$base_dir" && exit 1
 trap "{ rm -f "$base_dir/xpra-client.pid"; }" EXIT
-trap "{ log \"xpra_client.sh: trap triggered, ignoring\"; }" SIGINT SIGHUP
+trap "{ echo \"xpra_client.sh: trap triggered, ignoring\"; }" INT HUP
 1>"$base_dir/xpra-client.stdout" 2>"$base_dir/xpra-client.stderr" \
 xpra attach --socket-dir="$base_dir/control/xpra" --compressors= --compress=0 --encoding=rgb --speed=100 --tray=$enable_tray &
 xpra_pid="$!"
