256e95c7b9e73f0e3215b7feac06798eb77a4e1e
DarkCaster <dark.caster@outlook.com>
2017-04-16 04:09:40 +0300

    sandboxer: minor update in xpra_client.sh

--- Sandboxer/tools/xpra_client.sh
+++ Sandboxer/tools/xpra_client.sh
@@ -10,10 +10,10 @@ trap "{ rm -f "$base_dir/xpra-client.pid"; }" EXIT
 trap "{ echo \"xpra_client.sh: trap triggered, ignoring\"; }" INT HUP
 if test -z "$ipc_file"; then
   1>"$base_dir/xpra-client.stdout" 2>"$base_dir/xpra-client.stderr" \
-  xpra attach --socket-dir="$base_dir/control/xpra" --compressors= --compress=0 --encoding=rgb --speed=100 --tray=$enable_tray &
+  xpra attach --socket-dir="$base_dir/control/xpra" --compressors= --compress=0 --encoding=rgb --speed=100 --min-speed=100 --quality=100 --min-quality=100 --tray=$enable_tray &
 else
   1>"$base_dir/xpra-client.stdout" 2>"$base_dir/xpra-client.stderr" \
-  xpra attach --socket-dir="$base_dir/control/xpra" --mmap="$ipc_file" --compressors= --compress=0 --encoding=rgb --speed=100 --tray=$enable_tray &
+  xpra attach --socket-dir="$base_dir/control/xpra" --mmap="$ipc_file" --compressors= --compress=0 --encoding=rgb --speed=100 --min-speed=100 --quality=100 --min-quality=100 --tray=$enable_tray &
 fi
 xpra_pid="$!"
 echo "$xpra_pid" > "$base_dir/xpra-client.pid"
