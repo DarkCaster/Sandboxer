9baf700cc33367bdc944204065f5f618e2876c7f
DarkCaster <dark.caster@outlook.com>
2017-12-28 23:42:34 +0300

    create-debian-source.sh script updated: add support for modifying version suffix

--- create-debian-source.sh
+++ create-debian-source.sh
@@ -6,10 +6,14 @@ set -e
 curdir="$( cd "$( dirname "$0" )" && pwd )"
 
 target="$1"
-distro="$2"
-key="$3"
+key="$2"
+version="$3"
+distro="$4"
 
-[[ -z $target ]] && echo "usage: create-debian-source.sh <target directory> [distroseries] [sign key id]" && exit 1
+[[ -z $target ]] && echo "usage: create-debian-source.sh <target directory> [sign key id; \"none\" to skip] [version suffix (.ppa1~xenial for example); \"none\" to skip] [distroseries; \"none\" to use default \"unstable\"]" && exit 1
+[[ $key = none ]] && key=""
+[[ $version = none ]] && version=""
+[[ $distro = none ]] && distro=""
 
 mkdir -p "$target/sandboxer"
 
@@ -22,6 +26,7 @@ cd "$target/sandboxer"
 
 [[ -z $distro ]] && sed -i "s|__DISTRO__|unstable|g" "$target/sandboxer/debian/changelog"
 [[ ! -z $distro ]] && sed -i "s|__DISTRO__|""$distro""|g" "$target/sandboxer/debian/changelog"
+sed -i "s|__VERSION__SUFFIX__|""$version""|g" "$target/sandboxer/debian/changelog"
 
 if [[ -z $key ]]; then
   dpkg-buildpackage -d -S -us -uc
