8aa8316ec1494ceac98d5210dd10e5ed48866e12
DarkCaster <dark.caster@outlook.com>
2018-02-15 23:13:10 +0300

    Examples: rework and simplify steam.cfg.lua

--- Examples/steam.cfg.lua
+++ Examples/steam.cfg.lua
@@ -2,92 +2,31 @@
 -- using external debian chroot (debian jessie), use download-debian-jessie-chroot.sh to download and prepare rootfs archive.
 -- see steam-howto.txt for info about other preparations needed to install and run steam inside sandbox.
 
+-- using debian-sandbox.cfg.lua config file as base
+
 -- there may be some problems with 3d acceleration when using hardware that requires to install external driver and its own version of libGL
 -- for now this config is tested with Intel IGP that works with stock driers and libGL library.
 
-tunables.chrootdir=loader.path.combine(loader.workdir,"debian_chroot")
-dofile(loader.path.combine(loader.workdir,"debian-version-probe.lua.in"))
+-- redefine defaults.recalculate function, that will be called by base config
+defaults.recalculate_orig=defaults.recalculate
+function defaults.recalculate()
+  -- redefine some parameters
+  tunables.datadir=loader.path.combine(loader.workdir,"userdata-steam")
+  defaults.recalculate_orig()
+  defaults.mounts.resolvconf_mount=defaults.mounts.direct_resolvconf_mount
+end
 
-tunables.datadir=loader.path.combine(loader.workdir,"userdata-steam")
-tunables.etchost_path=loader.path.combine(tunables.chrootdir,"etc")
-tunables.features.dbus_search_prefix=tunables.chrootdir
-tunables.features.gvfs_fix_search_prefix=tunables.chrootdir
-tunables.features.pulse_env_alsa_config="skip"
-tunables.features.x11util_build=os_id.."-"..os_version.."-"..os_arch
 defaults.recalculate()
 
-sandbox={
-  features={
-    "dbus",
-    "gvfs_fix",
-    "pulse",
-    "x11host",
-    "envfix",
-  },
-  setup={
-    executor_build=os_id.."-"..os_version.."-"..os_arch,
-    commands={
-      defaults.commands.resolvconf,
-      defaults.commands.machineid_static,
-      defaults.commands.passwd,
-      defaults.commands.home,
-      defaults.commands.home_gui_config,
-      defaults.commands.var_cache,
-      defaults.commands.var_tmp,
-      {'mkdir -p "${cfg[tunables.auto.user_path]}/libs/i386"'},
-      {'mkdir -p "${cfg[tunables.auto.user_path]}/libs/x86_64"'},
-    },
-    env_whitelist={
-      "LANG",
-      "LC_ALL",
-    },
-    env_set={
-      {"PATH","/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games"},
-      defaults.env.set_xdg_runtime,
-      defaults.env.set_home,
-    },
-    mounts={
-      defaults.mounts.system_group, -- includes: proc, dev, empty /run dir, empty /var dir, empty /tmp
-      defaults.mounts.xdg_runtime_dir,
-      defaults.mounts.home_mount,
-      defaults.mounts.var_cache_mount,
-      defaults.mounts.var_tmp_mount,
-      defaults.mounts.var_lib_mount,
-      defaults.mounts.usr_ro_mount,
-      defaults.mounts.host_etc_mount,
-      defaults.mounts.passwd_mount,
-      defaults.mounts.machineid_mount,
-      defaults.mounts.resolvconf_mount,
-      defaults.mounts.devsnd_mount, -- for alsa. may be needed for some games
-      defaults.mounts.devdri_mount, -- needed for mesa and 3d
-      defaults.mounts.devinput_mount, -- joystics
-      defaults.mounts.sys_mount, -- needed for proper 3d support
-      --defaults.mounts.dbus_system_mount, -- enable to allow steam queries to network-manager (this should speedup steam startup, TODO: not tested)
-      --defaults.mounts.devshm_mount,
-    },
-  },
+-- load base config
+dofile(loader.path.combine(loader.workdir,"debian-sandbox.cfg.lua"))
 
-  bwrap={
-    defaults.bwrap.unshare_user,
-    --defaults.bwrap.unshare_ipc, -- ipc isolation must be disabled to use x11 and mesa hw-acceleration
-    defaults.bwrap.unshare_pid,
-    -- defaults.bwrap.unshare_net,
-    defaults.bwrap.unshare_uts,
-    -- defaults.bwrap.unshare_cgroup,
-    defaults.bwrap.uid,
-    defaults.bwrap.gid,
-  }
-}
+-- modify PATH env
+table.insert(sandbox.setup.env_set,{"PATH","/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games"})
 
--- add remaining mounts, depending on detected debian version
-add_debian_mounts()
-
--- add sbin mounts
-if os_version > os_oldfs_ver then
-  table.insert(sandbox.setup.mounts, {prio=15,"symlink","usr/sbin","sbin"})
-else
-  table.insert(sandbox.setup.mounts, defaults.mounts.sbin_ro_mount)
-end
+-- insert sandbox build commands
+table.insert(sandbox.setup.commands,{'mkdir -p "${cfg[tunables.auto.user_path]}/libs/i386"'})
+table.insert(sandbox.setup.commands,{'mkdir -p "${cfg[tunables.auto.user_path]}/libs/x86_64"'})
 
 shell={
   exec="/bin/bash",
