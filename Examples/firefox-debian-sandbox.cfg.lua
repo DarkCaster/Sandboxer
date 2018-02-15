aeea00bd4da718681aa7c3af1f8989d6065dbfff
DarkCaster <dark.caster@outlook.com>
2018-02-15 22:13:25 +0300

    fixup! Examples: rework and simplify firefox-debian-sandbox.cfg.lua

--- Examples/firefox-debian-sandbox.cfg.lua
+++ Examples/firefox-debian-sandbox.cfg.lua
@@ -1,102 +1,34 @@
 -- example config for firefox sandbox, which is created on top of external debian chroot, prepared by debian-setup.cfg.lua
+-- using debian-sandbox.cfg.lua config file as base
+
 -- xpra x11-forwarding software (must be installed on host, v2.0 and up) may be used to isolate sanbox from host x11 service.
 -- opengl acceleration untested and may not work (especially with xpra mode or when using proprietary video drivers that install it's own libgl).
--- this config is based on example.cfg.lua, most comments removed.
 
-tunables.chrootdir=loader.path.combine(loader.workdir,"debian_chroot")
-dofile(loader.path.combine(loader.workdir,"debian-version-probe.lua.in"))
+-- redefine defaults.recalculate function, that will be called by base config
+defaults.recalculate_orig=defaults.recalculate
+function defaults.recalculate()
+  -- redefine some parameters
+  tunables.datadir=loader.path.combine(loader.workdir,"userdata-firefox")
+  tunables.features.pulse_env_alsa_config="auto"
+  defaults.recalculate_orig()
+  defaults.mounts.resolvconf_mount=defaults.mounts.direct_resolvconf_mount
+end
 
-tunables.datadir=loader.path.combine(loader.workdir,"userdata-firefox")
-tunables.etchost_path=loader.path.combine(tunables.chrootdir,"etc")
-tunables.features.dbus_search_prefix=tunables.chrootdir
-tunables.features.xpra_search_prefix=tunables.chrootdir
-tunables.features.gvfs_fix_search_prefix=tunables.chrootdir
-tunables.features.pulse_env_alsa_config="auto"
-tunables.features.x11util_build=os_id.."-"..os_version.."-"..os_arch
 defaults.recalculate()
 
-sandbox={
-  features={
-    "dbus",
-    "gvfs_fix",
-    "pulse",
-    "x11host", -- less secure, try this if you do not have xpra software
-    --"xpra", -- more secure, you must install xpra software suite with server and client functionality.
-    "envfix",
-  },
-  setup={
-    executor_build=os_id.."-"..os_version.."-"..os_arch,
-    commands={
-      defaults.commands.resolvconf,
-      defaults.commands.machineid_static,
-      defaults.commands.passwd,
-      defaults.commands.home,
-      defaults.commands.home_gui_config,
-      defaults.commands.var_cache,
-      defaults.commands.var_tmp,
-    },
-    env_whitelist={
-      "LANG",
-      "LC_ALL",
-    },
-    env_set={
-      {"PATH","/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games"},
-      defaults.env.set_xdg_runtime,
-      defaults.env.set_home,
-    },
-    mounts={
-      defaults.mounts.system_group, -- includes: proc, dev, empty /run dir, empty /var dir, empty /tmp
-      defaults.mounts.xdg_runtime_dir,
-      defaults.mounts.home_mount,
-      defaults.mounts.var_cache_mount,
-      defaults.mounts.var_tmp_mount,
-      defaults.mounts.var_lib_mount,
-      defaults.mounts.usr_ro_mount,
-      defaults.mounts.host_etc_mount,
-      defaults.mounts.passwd_mount,
-      defaults.mounts.machineid_mount,
-      defaults.mounts.resolvconf_mount,
-      --defaults.mounts.devsnd_mount, -- for direct alsa support (alsa-pulse may work without it).
-      --defaults.mounts.devdri_mount, -- may be needed when using x11host for opengl acceleration
-      --defaults.mounts.sys_mount, -- may be needed when using x11host for opengl acceleration
-      --defaults.mounts.devinput_mount, -- joystics
-      --defaults.mounts.devshm_mount,
-    },
-  },
+-- load base config
+dofile(loader.path.combine(loader.workdir,"debian-sandbox.cfg.lua"))
 
-  bwrap={
-    defaults.bwrap.unshare_user,
-    -- defaults.bwrap.unshare_ipc,
-    defaults.bwrap.unshare_pid,
-    -- defaults.bwrap.unshare_net,
-    defaults.bwrap.unshare_uts,
-    -- defaults.bwrap.unshare_cgroup,
-    defaults.bwrap.uid,
-    defaults.bwrap.gid,
-  }
-}
+-- remove some mounts
+loader.table.remove_value(sandbox.setup.mounts,defaults.mounts.devsnd_mount)
+loader.table.remove_value(sandbox.setup.mounts,defaults.mounts.devdri_mount)
+loader.table.remove_value(sandbox.setup.mounts,defaults.mounts.devinput_mount)
+loader.table.remove_value(sandbox.setup.mounts,defaults.mounts.sys_mount)
+loader.table.remove_value(sandbox.setup.mounts,defaults.mounts.devshm_mount)
+loader.table.remove_value(sandbox.setup.mounts,defaults.mounts.sbin_ro_mount)
 
--- add remaining mounts, depending on detected debian version
-add_debian_mounts()
-
-shell={
-  exec="/bin/bash",
-  args={"-l"},
-  path="/",
-  env_set={
-    {"TERM",os.getenv("TERM")},
-  },
-  term_signal=defaults.signals.SIGHUP,
-  attach=true,
-  pty=true,
-  desktop={
-    name = "Shell for debian-firefox sandbox",
-    comment = "shell for sandbox uid "..config.sandbox_uid,
-    icon = "terminal",
-    terminal = true,
-    startupnotify = false,
-  },
-}
+-- modify PATH env
+table.insert(sandbox.setup.env_set,{"PATH","/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games"})
 
 firefox_log={
   exec="/usr/bin/firefox",
