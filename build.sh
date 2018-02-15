576192eb97c5f2171170ae2a73c4f7da44c134f3
DarkCaster <dark.caster@outlook.com>
2018-02-15 17:44:17 +0300

    install: build.sh script updated

--- build.sh
+++ build.sh
@@ -11,101 +11,65 @@ build="$1"
 
 curdir="$( cd "$( dirname "$0" )" && pwd )"
 
-function check_error {
- if [[ $? != 0 ]]; then
-  echo "Build ended with error !!!"
-  cd "$curdir"
-  exit 1
- fi
+set -eE
+function process_error() {
+  >&2 echo "command at line $1 has failed"
 }
+trap 'process_error $LINENO' ERR
 
 # Executor and Commander utils
-
 cd "$curdir"
 rm -rf "$curdir/Build/Executor-build"
-
-mkdir -p "$curdir/Build/Executor-build" && cd "$curdir/Build/Executor-build"
-check_error
-
+mkdir -p "$curdir/Build/Executor-build"
+cd "$curdir/Build/Executor-build"
 cmake -DCMAKE_INSTALL_PREFIX="$curdir/Build" -DCMAKE_BUILD_TYPE=Release ../../Executor
-check_error
-
 make
-check_error
-
 make install
-check_error
 
 # X11Util
-
 cd "$curdir"
 rm -rf "$curdir/Build/X11Util-build"
-
-mkdir -p "$curdir/Build/X11Util-build" && cd "$curdir/Build/X11Util-build"
-check_error
-
+mkdir -p "$curdir/Build/X11Util-build"
+cd "$curdir/Build/X11Util-build"
 cmake -DCMAKE_INSTALL_PREFIX="$curdir/Build" -DCMAKE_BUILD_TYPE=Release ../../X11Util
-check_error
-
 make
-check_error
-
 make install
-check_error
 
 # Fakeroot-UserNS
-
 mkdir -p "$curdir/External"
-check_error
-
 if [[ ! -d $curdir/External/Fakeroot-UserNS ]]; then
-  function check_error {
-   if [[ $? != 0 ]]; then
+  function process_error() {
     echo "Fakeroot-UserNS configuration failed !!!"
     cd "$curdir"
     rm -rf "$curdir/External/Fakeroot-UserNS"
-    exit 1
-   fi
   }
+  rm -rf "$curdir/External/Fakeroot-UserNS"
   cd "$curdir/External"
   git clone https://github.com/DarkCaster/Fakeroot-UserNS.git
-  check_error
   cd "$curdir/External/Fakeroot-UserNS"
-  check_error
   patch -p1 -i ./debian/patches/eglibc-fts-without-LFS
-  check_error
   patch -p1 -i ./debian/patches/glibc-xattr-types
-  check_error
   patch -p1 -i ./debian/patches/fix-shell-in-fakeroot
-  check_error
   patch -p1 -i ./debian/patches/hide-dlsym-error.patch
   ./preroll
-  check_error
+  function process_error() {
+    >&2 echo "command at line $1 has failed"
+  }
 fi
 
 rm -rf "$curdir/Build/Fakeroot-UserNS-build"
 rm -rf "$curdir/Build/fixups/fakeroot"/*
-
-mkdir -p "$curdir/Build/Fakeroot-UserNS-build" && cd "$curdir/Build/Fakeroot-UserNS-build"
-check_error
+mkdir -p "$curdir/Build/Fakeroot-UserNS-build"
+cd "$curdir/Build/Fakeroot-UserNS-build"
 
 if [[ -z $build ]]; then
   "$curdir/External/Fakeroot-UserNS/configure" --prefix="/fixups/fakeroot-host" --bindir="/fixups/fakeroot-host" --libdir="/fixups/fakeroot-host" --mandir="/fixups/fakeroot-host/man" --with-ipc=tcp
-  check_error
 else
   "$curdir/External/Fakeroot-UserNS/configure" --prefix="/fixups/fakeroot-$build" --bindir="/fixups/fakeroot-$build" --libdir="/fixups/fakeroot-$build" --mandir="/fixups/fakeroot-$build/man" --with-ipc=tcp
-  check_error
 fi
 
 make
-check_error
-
 make install DESTDIR="$curdir/Build"
-check_error
-
 cd "$curdir"
 
-if [[ ! -f $curdir/BashLuaHelper/lua-helper.bash.in ]]; then
-  git submodule update --init
-  check_error
-fi
+[[ ! -f $curdir/BashLuaHelper/lua-helper.bash.in ]] && git submodule update --init
