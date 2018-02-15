55a9a2a84ba6d50c4822f938c8f6fd95bb306d4a
DarkCaster <dark.caster@outlook.com>
2018-02-15 23:36:15 +0300

    Examples: rework and simplify teamviewer-debian-sandbox.cfg.lua

--- Examples/teamviewer-debian-sandbox.cfg.lua
+++ Examples/teamviewer-debian-sandbox.cfg.lua
@@ -1,105 +1,32 @@
 -- example config for teamviewer sandbox.
 -- you need to use i386-based debian chroot downloaded with download-debian-stretch-i386-chroot.sh script and prepared with debian-setup.cfg.lua config.
--- if using x86_64 host env you must also use i386 session management utilities: https://github.com/DarkCaster/Sandboxer/releases/tag/external-binaries .
--- (archives downloaded from link above should be extracted to ~/.cache/sandboxer directory).
--- TODO: add helper script for automatic download of precompiled binaries.
+-- using debian-sandbox.cfg.lua config file as base
+
+-- if using x86_64 host env you must also use i386 session management utilities by running "sandboxer-download-extra" command.
 -- you must also manually install libpng12-0 package from debian jessie (it was removed in stretch distro).
 -- see https://packages.debian.org/jessie/libpng12-0 for download links, and install package into debian chroot manually with "dpkg -i" command.
 
--- this config is based on example.cfg.lua, most comments removed.
-
--- chroot directory name, relative to this config file directory
-tunables.chrootdir=loader.path.combine(loader.workdir,"debian_chroot")
-
--- detect debian\ubuntu os version
-dofile(loader.path.combine(loader.workdir,"debian-version-probe.lua.in"))
-
-tunables.etchost_path=loader.path.combine(tunables.chrootdir,"etc")
-tunables.features.dbus_search_prefix=tunables.chrootdir
-tunables.features.xpra_search_prefix=tunables.chrootdir
-tunables.features.gvfs_fix_search_prefix=tunables.chrootdir
-tunables.features.pulse_env_alsa_config="skip"
-tunables.features.x11util_build=os_id.."-"..os_version.."-"..os_arch
+-- redefine defaults.recalculate function, that will be called by base config
+defaults.recalculate_orig=defaults.recalculate
+function defaults.recalculate()
+  defaults.recalculate_orig()
+  defaults.mounts.resolvconf_mount=defaults.mounts.direct_resolvconf_mount
+end
 
 defaults.recalculate()
 
-sandbox={
-  -- sandbox features and host-integration stuff that require some complex or dynamic preparations.
-  -- features are enabled in order of appearance, feature name may contain only lowercase letters, numbers and underscores.
-  features={
-    --"dbus", -- looks like it is not needed, at least for outgoing sessions
-    "gvfs_fix",
-    "pulse",
-    "x11host",
-    "envfix",
-  },
-  setup={
-    executor_build=os_id.."-"..os_version.."-"..os_arch,
-    commands={
-      defaults.commands.machineid_static,
-      defaults.commands.passwd,
-      defaults.commands.resolvconf,
-      defaults.commands.home,
-      defaults.commands.home_gui_config,
-      defaults.commands.var_cache,
-      defaults.commands.var_tmp,
-    },
-    env_whitelist={
-      "LANG",
-      "LC_ALL",
-    },
-    env_set={
-      {"PATH","/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"},
-      defaults.env.set_xdg_runtime,
-      defaults.env.set_home,
-    },
-    mounts={
-      defaults.mounts.system_group, -- includes: proc, dev, empty /run dir, empty /var dir, empty /tmp
-      defaults.mounts.xdg_runtime_dir,
-      defaults.mounts.home_mount,
-      defaults.mounts.var_cache_mount,
-      defaults.mounts.var_tmp_mount,
-      defaults.mounts.var_lib_mount,
-      -- other dirs from our external chroot
-      defaults.mounts.usr_ro_mount,
-      defaults.mounts.host_etc_mount,
-      defaults.mounts.passwd_mount,
-      defaults.mounts.machineid_mount,
-      defaults.mounts.resolvconf_mount,
-      -- optional mounts, may be useful for some programs
-      --defaults.mounts.devsnd_mount,
-      --defaults.mounts.devdri_mount,
-      --defaults.mounts.devinput_mount,
-      --defaults.mounts.sys_mount,
-    },
-  },
+-- load base config
+dofile(loader.path.combine(loader.workdir,"debian-sandbox.cfg.lua"))
 
-  bwrap={
-    defaults.bwrap.unshare_user,
-    -- defaults.bwrap.unshare_ipc,
-    defaults.bwrap.unshare_pid,
-    -- defaults.bwrap.unshare_net,
-    defaults.bwrap.unshare_uts,
-    -- defaults.bwrap.unshare_cgroup,
-    defaults.bwrap.uid,
-    defaults.bwrap.gid,
-  }
-}
+-- remove some unneded features and mounts
+loader.table.remove_value(sandbox.features,"dbus")
 
--- add remaining mounts, depending on detected debian version
-add_debian_mounts()
-
-shell={
-  exec="/bin/bash",
-  args={"-l"},
-  path="/",
-  env_set={
-    {"TERM",os.getenv("TERM")},
-  },
-  term_signal=defaults.signals.SIGHUP,
-  attach=true,
-  pty=true,
-}
+-- remove some mounts from base config
+loader.table.remove_value(sandbox.setup.mounts,defaults.mounts.devsnd_mount)
+loader.table.remove_value(sandbox.setup.mounts,defaults.mounts.devdri_mount)
+loader.table.remove_value(sandbox.setup.mounts,defaults.mounts.sys_mount)
+loader.table.remove_value(sandbox.setup.mounts,defaults.mounts.devinput_mount)
+loader.table.remove_value(sandbox.setup.mounts,defaults.mounts.devshm_mount)
 
 teamviewer={
   exec="/home/sandboxer/teamviewer/teamviewer",
