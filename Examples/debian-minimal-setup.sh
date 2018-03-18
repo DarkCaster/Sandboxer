c3e026f323adc95687a41f99a9f03203c94e6cfa
DarkCaster <dark.caster@outlook.com>
2018-03-19 02:41:57 +0300

    Examples: debian-minimal-setup.sh script updated, fix error with adwaita-icon-theme-full package selection

--- Examples/debian-minimal-setup.sh
+++ Examples/debian-minimal-setup.sh
@@ -3,46 +3,20 @@
 # perform some basic setup for fresh debian_chroot
 # intended for run with debian-setup profile
 
-check_errors () {
-  local status="$?"
-  local msg="$@"
-  if [ "$status" != "0" ]; then
-    echo "ERROR: operation finished with error code $status"
-    [ ! -z "$msg" ] && echo "$msg"
-    exit "$status"
-  fi
-}
+set -e
 
+# minimal setup
 apt-get update
-check_errors
-
 apt-get install dialog -y
-check_errors
-
 apt-get install locales -y
-check_errors
-
 dpkg-reconfigure locales
-check_errors
-
 apt-get install apt-utils -y
-check_errors
-
 dpkg-reconfigure dialog
-check_errors
-
 dpkg-reconfigure apt-utils
-check_errors
-
 apt-get install tzdata -y
-check_errors
-
 dpkg-reconfigure tzdata
-check_errors
+apt-get dist-upgrade -y
 
 # additional packages for dbus, x11, mesa, pulse integration and package management gui (synaptic) with minimal x11 stack
-apt-get install -y net-tools mc libpulse0 mesa-utils lsb-release wget adwaita-icon-theme-full synaptic xauth gnupg2 apt-transport-https
-check_errors
-
-apt-get dist-upgrade -y
-check_errors
+apt-get install -y net-tools mc libpulse0 mesa-utils lsb-release wget adwaita-icon-theme synaptic xauth gnupg2 apt-transport-https
+apt-get install -y adwaita-icon-theme-full || true
