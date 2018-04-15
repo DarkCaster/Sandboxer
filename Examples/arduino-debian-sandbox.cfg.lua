685c325458e542a691402f7f072bffea59c889ee
DarkCaster <dark.caster@outlook.com>
2018-04-15 15:33:20 +0300

    Examples: add arduino-debian-sandbox.cfg.lua example config

---
+++ Examples/arduino-debian-sandbox.cfg.lua
@@ -0,0 +1,58 @@
+-- experimental, not for regular use!
+-- example config for arduino-ide sandbox, which is created on top of external debian chroot, prepared by debian-setup.cfg.lua
+-- using debian-sandbox.cfg.lua config file as base
+
+-- redefine defaults.recalculate function, that will be called by base config
+defaults.recalculate_orig=defaults.recalculate
+function defaults.recalculate()
+  -- redefine some parameters
+  tunables.datadir=loader.path.combine(loader.workdir,"userdata-arduino")
+  defaults.recalculate_orig()
+  defaults.mounts.resolvconf_mount=defaults.mounts.direct_resolvconf_mount
+end
+
+defaults.recalculate()
+
+-- load base config
+dofile(loader.path.combine(loader.workdir,"debian-sandbox.cfg.lua"))
+
+-- remove some unneded features and mounts
+loader.table.remove_value(sandbox.features,"pulse")
+
+-- remove some mounts from base config
+loader.table.remove_value(sandbox.setup.mounts,defaults.mounts.devsnd_mount)
+loader.table.remove_value(sandbox.setup.mounts,defaults.mounts.devdri_mount)
+loader.table.remove_value(sandbox.setup.mounts,defaults.mounts.sys_mount)
+loader.table.remove_value(sandbox.setup.mounts,defaults.mounts.devinput_mount)
+loader.table.remove_value(sandbox.setup.mounts,defaults.mounts.devshm_mount)
+loader.table.remove_value(sandbox.setup.mounts,defaults.mounts.sbin_ro_mount)
+
+-- modify PATH env
+table.insert(sandbox.setup.env_set,{"PATH","/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games"})
+
+-- remove unshare_ipc bwrap param
+loader.table.remove_value(sandbox.bwrap,defaults.bwrap.unshare_ipc)
+
+arduino_install={
+  exec="/home/sandboxer/arduino/install.sh",
+  path="/home/sandboxer/arduino",
+  attach=true,
+  pty=false,
+}
+
+arduino={
+  exec="/home/sandboxer/arduino/arduino",
+  path="/home/sandboxer/arduino",
+  args=loader.args,
+  term_signal=defaults.signals.SIGTERM,
+  attach=false,
+  pty=false,
+  desktop={
+    name = "Arduino IDE (in sandbox)",
+    comment = "Arduino IDE, sandbox uid "..config.sandbox_uid,
+    icon = loader.path.combine(tunables.datadir,"/home/sandboxer/.local/share/icons/hicolor/128x128/apps/arduino-arduinoide.png"),
+    terminal = false,
+    startupnotify = false,
+    categories="Development;IDE;Electronics",
+  },
+}
