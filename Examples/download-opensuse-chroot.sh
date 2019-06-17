0dd790966631a1073281eabf4255cb9a01c65dc7
DarkCaster <dark.caster@outlook.com>
2019-06-17 12:05:06 +0300

    Examples: download-opensuse-chroot.sh script updated - added support for openSUSE 15.0 and 15.1, download root-fs archives from OBS

--- Examples/download-opensuse-chroot.sh
+++ Examples/download-opensuse-chroot.sh
@@ -6,7 +6,7 @@
 script_dir="$( cd "$( dirname "$0" )" && pwd )"
 
 show_usage() {
-  echo "usage: download-debian-chroot.sh <distro's major version number or codename> [arch. only i386 and amd64 (default) supported now]"
+  echo "usage: download-debian-chroot.sh <distro's major version number or codename> [arch. only i686 and amd64 (default) supported now. i686 may be selected only for tumbleweed (experimental)]"
   exit 1
 }
 
@@ -14,7 +14,7 @@ set -e
 
 arch="$2"
 [[ -z $arch ]] && arch="amd64"
-[[ $arch != amd64 && $arch != i386 ]] && \
+[[ $arch != amd64 && $arch != i686 ]] && \
   echo "selected arch $arch is not supported for now and may not work with sandboxer!" && \
   exit 1
 
@@ -26,9 +26,26 @@ case "$name" in
   "42.3")
     name="42.3"
     arch="amd64"
+    url="https://download.opensuse.org/repositories/Virtualization:/containers:/images:/openSUSE-Leap-42.3/containers/opensuse-leap-image.x86_64-lxc.tar.xz"
+  ;;
+  "15.0")
+    name="15.0"
+    arch="amd64"
+    url="https://download.opensuse.org/repositories/Virtualization:/containers:/images:/openSUSE-Leap-15.0/containers/opensuse-leap-image.x86_64-lxc.tar.xz"
+  ;;
+  "15.1")
+    name="15.1"
+    arch="amd64"
+    url="https://download.opensuse.org/repositories/Virtualization:/containers:/images:/openSUSE-Leap-15.1/containers/opensuse-leap-image.x86_64-lxc.tar.xz"
   ;;
   "tumbleweed")
     name="tumbleweed"
+    url=""
+    if [[ $arch = amd64 ]]; then
+      url="https://download.opensuse.org/repositories/Virtualization:/containers:/images:/openSUSE-Tumbleweed/container/opensuse-tumbleweed-image.x86_64-lxc.tar.xz"
+    elif [[ $arch = i686 ]]; then
+      url="https://download.opensuse.org/repositories/Virtualization:/containers:/images:/openSUSE-Tumbleweed/container/opensuse-tumbleweed-image.i686-lxc.tar.xz"
+    fi
   ;;
   *)
     echo "selected opensuse distro name or version is not supported. supported versions include: 42.3, tumbleweed"
@@ -36,7 +53,37 @@ case "$name" in
   ;;
 esac
 
-"$script_dir/download-image-from-docker-repo.sh" opensuse "$name" "$arch"
+[[ -d "$script_dir/opensuse_chroot" ]] && echo "opensuse_chroot dir already exist in script dir, please remove it before running this script"
+
+tmp_dir=`mktemp -d -t suse-XXXXXXXXX`
+wget -O "$tmp_dir/rootfs.tar.xz" "$url"
+
+mkdir -p "$script_dir/opensuse_chroot"
+pushd "$script_dir/opensuse_chroot"
+
+xz -d -c "$tmp_dir/rootfs.tar.xz" | tar xf - --no-same-owner --preserve-permissions --exclude='dev'
+
+# write arch-label file
+[[ ! -z $arch ]] && echo "$arch" > "arch-label"
+
+# create boot directory if missing
+mkdir -p ./boot
+
+# remove machine-id, will be generated automatically
+rm -f ./etc/machine-id
+
+# check for merged root-fs layout, and mark it
+# see https://wiki.debian.org/UsrMerge for more info
+fs_layout="merged"
+[[ -d ./bin && ! -L ./bin ]] && fs_layout="normal"
+[[ -d ./sbin && ! -L ./sbin ]] && fs_layout="normal"
+[[ -d ./lib && ! -L ./lib ]] && fs_layout="normal"
+[[ -d ./lib32 && ! -L ./lib32 ]] && fs_layout="normal"
+[[ -d ./lib64 && ! -L ./lib64 ]] && fs_layout="normal"
+[[ -d ./libx32 && ! -L ./libx32 ]] && fs_layout="normal"
+echo "$fs_layout" > "fs-layout"
+
+popd
 
 #remove "installrecommends = no" for zypper, that was defined on image builds
 sed -i -e 's|^installRecommends.*$|# installRecommends = yes|g' "$script_dir/opensuse_chroot/etc/zypp/zypper.conf"
