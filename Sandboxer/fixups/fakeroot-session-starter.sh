25eeed69c1691d471f8bb80ce65dbd18974839ec
DarkCaster <dark.caster@outlook.com>
2019-06-18 23:20:50 +0300

    sandboxer: fakeroot-session-starter.sh script updated - more informative output

--- Sandboxer/fixups/fakeroot-session-starter.sh
+++ Sandboxer/fixups/fakeroot-session-starter.sh
@@ -21,24 +21,24 @@ test -z "$fakeroot_version" -o -z "$command" && show_usage
 test -z "$fakeroot_arch" -o -z "$command" && show_usage
 
 fakeroot=""
-echo "fakeroot-session-starter: trying to detect requested build '$fakeroot_dist-$fakeroot_version-$fakeroot_arch' of fakeroot utility"
 for hint in "$fakeroot_search_dir/fakeroot-$fakeroot_dist-$fakeroot_version-$fakeroot_arch" "$fakeroot_search_dir/fakeroot-$fakeroot_dist-$fakeroot_arch" "$fakeroot_search_dir/fakeroot-host"
 do
+  echo -n "searching for fakeroot utility at $hint dir ... "
   if [ -x "$hint/fakeroot" ]; then
     fakeroot="$hint/fakeroot"
-    echo "fakeroot-session-starter: using fakeroot binary at $hint directory"
+    echo "found!"
     break
   else
-    echo "fakeroot-session-starter: probe for fakeroot at $hint directory failed! you should try to download extra prebuilt fakeroot binaries by running sandboxer-download-extra.sh"
+    echo "not found..."
   fi
 done
 
-test -z "$fakeroot" && echo "fakeroot binaries not found! cannot proceed." && exit 1
+test -z "$fakeroot" && echo "usable fakeroot binary not found! cannot proceed..." && exit 1
 
 # In some host/sandbox environments fakeroot utility refuse to work on the first run, so we will use this small and dirty hack for now.
 2>/dev/null "$fakeroot" -- /bin/true
 "$fakeroot" -- "$command" "$@"
 
 ec="$?"
-echo "fakeroot-session-starter: fakeroot utility exit with code $ec, fakeroot-session-starter shuting down"
+echo "fakeroot utility exit code $ec, fakeroot-session-starter shuting down"
 exit $ec
