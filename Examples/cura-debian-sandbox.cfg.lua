d39e333f397e389c40916841aedceee683c23894
DarkCaster <dark.caster@outlook.com>
2020-09-07 00:37:28 +0300

    Examples: cura example updated

--- Examples/cura-debian-sandbox.cfg.lua
+++ Examples/cura-debian-sandbox.cfg.lua
@@ -1,6 +1,9 @@
 -- example config for ultimaker cura sandbox, which is created on top of external debian chroot, prepared by debian-setup.cfg.lua
 -- using debian-sandbox.cfg.lua config file as base
 
+-- if using nvidia GPU, you also need to install same proprietery nvidia driver version into sandbox,
+-- also you need to install libc6-dev and libglvnd-dev (https://community.ultimaker.com/topic/17302-i-keep-getting-segfaults-when-running-cura)
+
 -- redefine defaults.recalculate function, that will be called by base config
 defaults.recalculate_orig=defaults.recalculate
 function defaults.recalculate()
@@ -41,6 +44,10 @@ cura={
   attach=true,
   pty=false,
   exclusive=true,
+  env_set={ -- fix for https://stackoverflow.com/questions/10396141/strange-unicodeencodeerror-using-os-path-exists
+    {"LANG","en_US.UTF-8"},
+    {"LC_ALL","en_US.UTF-8"},
+  },
   desktop={
     name = "Ultimaker Cura (in sandbox)",
     comment = "Ultimaker Cura, sandbox uid "..config.sandbox_uid,
@@ -51,10 +58,21 @@ cura={
   },
 }
 
+cura_install_old_appimage={
+  exec="/bin/bash",
+  path="/home/sandboxer",
+  args={"-c","img=`find \"$HOME/installs\" -name \"Cura-*.AppImage\"|sort -V|tail -n1` && echo \"installing $img\" && rm -rf $HOME/Cura && mkdir $HOME/Cura && cd $HOME/Cura && bsdtar xvfp \"$img\""},
+  term_signal=defaults.signals.SIGTERM,
+  attach=true,
+  pty=false,
+  exclusive=true,
+}
+
 cura_install_appimage={
   exec="/bin/bash",
   path="/home/sandboxer",
   args={"-c","img=`find \"$HOME/installs\" -name \"Cura-*.AppImage\"|sort -V|tail -n1` && echo \"installing $img\" && rm -rf $HOME/Cura && mkdir $HOME/Cura && cd $HOME/Cura && bsdtar xvfp \"$img\""},
+  args={"-c","rm -rf $HOME/Cura && img=`find ./installs -name \"Ultimaker_Cura-*.AppImage\"|sort -V|tail -n1` && chmod 755 $img && $img --appimage-extract && mv $HOME/squashfs-root $HOME/Cura"},
   term_signal=defaults.signals.SIGTERM,
   attach=true,
   pty=false,
