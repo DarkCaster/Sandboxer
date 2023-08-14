d0f6446dae7e217e2b0c39791e2a07ca6f466efb
DarkCaster <dark.caster@outlook.com>
2023-08-14 03:42:22 +0300

    Examples: remove blender and freecad examples, add 3d-modeling-debian-sandbox.cfg.lua example config

--- Examples/3d-modeling-debian-sandbox.cfg.lua
+++ Examples/3d-modeling-debian-sandbox.cfg.lua
@@ -1,13 +1,12 @@
--- example config for freecad sandbox, which is created on top of external debian chroot, prepared by debian-setup.cfg.lua
+-- example config for sandbox with various 3d modelling software, which is created on top of external debian chroot, prepared by debian-setup.cfg.lua
 -- using debian-sandbox.cfg.lua config file as base
 
 -- redefine defaults.recalculate function, that will be called by base config
 defaults.recalculate_orig=defaults.recalculate
 function defaults.recalculate()
   -- redefine some parameters
-  tunables.datadir=loader.path.combine(loader.workdir,"userdata-freecad")
+  tunables.datadir=loader.path.combine(loader.workdir,"userdata-3d-modeling")
   defaults.recalculate_orig()
-  defaults.mounts.resolvconf_mount=defaults.mounts.direct_resolvconf_mount
 end
 
 defaults.recalculate()
@@ -17,6 +16,10 @@ dofile(loader.path.combine(loader.workdir,"debian-sandbox.cfg.lua"))
 
 -- remove some unneded features and mounts
 loader.table.remove_value(sandbox.features,"pulse")
+
+-- enable resolvconf feature
+table.insert(sandbox.features,"resolvconf")
+loader.table.remove_value(sandbox.setup.mounts,defaults.mounts.resolvconf_mount)
 
 -- remove some mounts from base config
 loader.table.remove_value(sandbox.setup.mounts,defaults.mounts.devsnd_mount)
@@ -35,6 +38,34 @@ table.insert(sandbox.setup.mounts,{prio=99,"bind-try",loader.path.combine(loader
 
 -- remove unshare_ipc bwrap param
 loader.table.remove_value(sandbox.bwrap,defaults.bwrap.unshare_ipc)
+
+blender={
+  exec="/home/sandboxer/blender/blender-launcher",
+  path="/home/sandboxer/blender",
+  args=loader.args,
+  term_signal=defaults.signals.SIGTERM,
+  attach=true,
+  pty=false,
+  exclusive=true,
+  desktop={
+    name = "Blender (in sandbox)",
+    comment = "Blender, sandbox uid "..config.sandbox_uid,
+    icon = "blender",
+    terminal = false,
+    startupnotify = false,
+    categories="Graphics;",
+  },
+}
+
+blender_install={
+  exec="/bin/bash",
+  path="/home/sandboxer",
+  args={"-c","rm -rf $HOME/blender && img=`find ./installs -name \"blender-*-linux-x64.tar.xz\"|sort -V|tail -n1` && echo \"extracting $img\" && cp \"$img\" /tmp/blender.tar.xz && cd /tmp && tar xf /tmp/blender.tar.xz && mv blender-*-linux-* $HOME/blender"},
+  term_signal=defaults.signals.SIGTERM,
+  attach=true,
+  pty=false,
+  exclusive=true,
+}
 
 freecad={
   exec="/home/sandboxer/Freecad/AppRun",
@@ -95,3 +126,4 @@ openscad_install_appimage={
 }
 
 openscad_install=openscad_install_appimage
+
