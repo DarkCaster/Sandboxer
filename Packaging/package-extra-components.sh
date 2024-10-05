9c15a3d08d2b8a79e8f2006b4787373b2d9b92ac
DarkCaster <dark.caster@outlook.com>
2024-10-05 06:47:44 +0300

    Packaging: package-extra-components.sh script updated

--- Packaging/package-extra-components.sh
+++ Packaging/package-extra-components.sh
@@ -2,52 +2,29 @@
 
 # package binary components, build by build.sh script
 
-#TODO
-echo "TODO: update this script to use with travis-ci"
-exit 1
+set -e
 
 build="$1"
 
 [[ -z $build ]] && echo "usage: <build id>" && exit 1
 
-curdir="$( cd "$( dirname "$0" )" && pwd )"
-
-function check_error {
- if [[ $? != 0 ]]; then
-  echo "Build ended with error !!!"
-  cd "$curdir"
-  exit 1
- fi
-}
-
-cd "$curdir/Build"
-check_error
+srcdir="$(cd "$(dirname "$0")" && pwd)"
+cd "$srcdir/../Build"
 
 #get source-checksum
-source_checksum=`2>/dev/null "commander/commander"`
+source_checksum=$("commander/commander" 2>/dev/null || true)
 [[ -z $source_checksum ]] && echo "failed to read correct source_checksum from commander!" && exit 1
 
 rm -rf "executor-$build-$source_checksum"
-check_error
-
 cp -r "executor" "executor-$build-$source_checksum"
-check_error
 
 rm -rf "x11util-$build"
-check_error
-
 cp -r "x11util" "x11util-$build"
-check_error
 
 [[ ! -d fixups/fakeroot-$build ]] && echo "fakeroot-$build not found. Run \"./build.sh\" script with \"$build\" parameter" && exit 1
 
 rm -rf "fakeroot-$build"
-check_error
-
 cp -r "fixups/fakeroot-$build" "fakeroot-$build"
-check_error
-
-# TODO: sign archives
 
 tar cf "executor-$build-$source_checksum.tar" "executor-$build-$source_checksum" --owner=0 --group=0
 xz -9e "executor-$build-$source_checksum.tar"
