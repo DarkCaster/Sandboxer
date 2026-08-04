dded26fff8825e5d4653af644f147c14cba70e11
DarkCaster <dark.caster@outlook.com>
2026-08-04 03:58:56 +0300

    Examples: pi-agent config example updated

--- Examples/pi-agent.cfg.lua
+++ Examples/pi-agent.cfg.lua
@@ -38,7 +38,7 @@ table.insert(sandbox.bwrap,defaults.bwrap.hostname_sandbox)
 
 -- modify env
 table.insert(sandbox.setup.env_set,{"PATH","/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games"})
-table.insert(sandbox.setup.env_set,{"CHROME_EXECUTABLE","/usr/bin/microsoft-edge"})
+-- table.insert(sandbox.setup.env_set,{"CHROME_EXECUTABLE","/usr/bin/microsoft-edge"})
 
 -- add mount for the ~/installs dir if present
 table.insert(sandbox.setup.mounts,{prio=99,"ro-bind-try",loader.path.combine(loader.workdir,"installs"),"/home/pi/installs"})
@@ -52,3 +52,78 @@ table.insert(sandbox.setup.mounts,{prio=99,"ro-bind-try",loader.path.combine(loa
 shell.term_orphans=true
 shell.env_unset={"MAIL"}
 shell.env_set={{"TERM",os.getenv("TERM")},{"LANG","en_US.UTF-8"},{"LC_ALL","en_US.UTF-8"},{"TZ","GMT+0"}}
+
+nvm_curl_install={
+  exec="/bin/bash",
+  path="/home/pi",
+  args={"-lic", "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.6/install.sh | bash"},
+  env_unset={"MAIL"},
+  env_set={{"TERM",os.getenv("TERM")},{"LANG","en_US.UTF-8"},{"LC_ALL","en_US.UTF-8"},{"TZ","GMT+0"}},
+  term_signal=defaults.signals.SIGTERM,
+  attach=true,
+  pty=true,
+  exclusive=true,
+}
+
+node24_nvm_install={
+  exec="/bin/bash",
+  path="/home/pi",
+  args={"-lic", "nvm install 24"},
+  env_unset={"MAIL"},
+  env_set={{"TERM",os.getenv("TERM")},{"LANG","en_US.UTF-8"},{"LC_ALL","en_US.UTF-8"},{"TZ","GMT+0"}},
+  term_signal=defaults.signals.SIGTERM,
+  attach=true,
+  pty=true,
+  exclusive=true,
+}
+
+pi_npm_install={
+  exec="/bin/bash",
+  path="/home/pi",
+  args={"-lic", "echo \"updating npm\" && npm install -g npm@latest && echo \"updating packages\" && npm update -g && echo \"installing pi\" && npm install -g --ignore-scripts @earendil-works/pi-coding-agent"},
+  env_unset={"MAIL"},
+  env_set={{"TERM",os.getenv("TERM")},{"LANG","en_US.UTF-8"},{"LC_ALL","en_US.UTF-8"},{"TZ","GMT+0"}},
+  term_signal=defaults.signals.SIGTERM,
+  attach=true,
+  pty=true,
+  exclusive=true,
+}
+
+pi_curl_install={
+  exec="/bin/bash",
+  path="/home/pi",
+  args={"-lic", "curl -fsSL https://pi.dev/install.sh | sh"},
+  env_unset={"MAIL"},
+  env_set={{"TERM",os.getenv("TERM")},{"LANG","en_US.UTF-8"},{"LC_ALL","en_US.UTF-8"},{"TZ","GMT+0"}},
+  term_signal=defaults.signals.SIGTERM,
+  attach=true,
+  pty=true,
+  exclusive=true,
+}
+
+-- may need to install fd-find and ripgrep packages
+pi={
+  exec="/bin/bash",
+  path="/home/pi",
+  args={"-lic", "pi && clear"},
+  env_unset={"MAIL"},
+  env_set={{"TERM",os.getenv("TERM")},{"LANG","en_US.UTF-8"},{"LC_ALL","en_US.UTF-8"},{"TZ","GMT+0"}},
+  term_signal=defaults.signals.SIGTERM,
+  attach=true,
+  pty=true,
+  exclusive=true,
+  term_orphans=true,
+}
+
+pi_resume={
+  exec="/bin/bash",
+  path="/home/pi",
+  args={"-lic", "pi -r && clear"},
+  env_unset={"MAIL"},
+  env_set={{"TERM",os.getenv("TERM")},{"LANG","en_US.UTF-8"},{"LC_ALL","en_US.UTF-8"},{"TZ","GMT+0"}},
+  term_signal=defaults.signals.SIGTERM,
+  attach=true,
+  pty=true,
+  exclusive=true,
+  term_orphans=true,
+}
